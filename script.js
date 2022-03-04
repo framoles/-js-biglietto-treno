a474af06f5cb3e79c39af72eaee1e7219428c94a
Francesco Moles <francesco.moles@outlook.it>
2022-03-04 16:35:40 +0100

    bug fix

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
-const km = prompt("Quanti km devi percorrere");
-const age = prompt("Quanti anni hai?");
+const km = parseInt(prompt("Quanti km devi percorrere"));
+const age = parseInt(prompt("Quanti anni hai?"));
 
-const costkm = km * 0.21;
-
-let discount;
-
-if (age < 18) {
-    discount = costkm - (costkm * 0.2);
-}
-else if (age > 65) {
-    discount = costkm - (costkm * 0.4);
+if (isNaN(km) || isNaN(age)) {
+    alert("Devi inserire un numero")
 }
 else {
-    discount = costkm;
-}
 
-document.getElementById("price").innerHTML = discount.toFixed(2);
+    const costkm = km * 0.21;
+
+    let discount;
+
+    if (age < 18) {
+        discount = costkm - (costkm * 0.2);
+    }
+    else if (age > 65) {
+        discount = costkm - (costkm * 0.4);
+    }
+    else {
+        discount = costkm;
+    }
+
+    document.getElementById("price").innerHTML = discount.toFixed(2);
+}
